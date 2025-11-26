b5653b408c0cca94e733dbcfddb658d3d1a95484
Mislav Češnik <mislav.cesnik88@gmail.com>
2025-11-26 20:25:33 +0100

    Initial commit

---
+++ src/test/java/com/mcesnik/planner_backend/PlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcesnik.planner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
